3dcffd5ea74cbf9fd677306a3e73191349d23088
Pranav Kumar <pranavtat09@gmail.com>
2020-05-12 23:08:57 +0530

    Lab531

---
+++ JavaForProgrammer/src/com/pranav/corejava/Lab531.java
@@ -0,0 +1,26 @@
+package com.pranav.corejava;
+
+class Lab531 {
+	public static void main(String[] args) {
+		Customer c1 = new Customer();
+		c1.show();
+		Customer c2 = new Customer();
+		c2.cid = 99;
+		c2.cname = "Pranav Kumar";
+		c2.phone = 765467899;
+		c2.show();
+	}
+}
+
+class Customer {
+	int cid;
+	String cname;
+	long phone;
+
+	void show() {
+
+		System.out.println(cid);
+		System.out.println(cname);
+		System.out.println(phone);
+	}
+}
